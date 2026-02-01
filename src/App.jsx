be0619169c9f57ec0c104f8a2c91911bf117e1b6
Rosuelo996 <rosuelo996.sr@gmail.com>
2026-02-01 19:07:45 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,41 @@
-import React, { useEffect, useState } from "react";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
-import Player from "./pages/Player/Player";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";
+import React, { useEffect } from 'react'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import { Routes, Route, useNavigate} from 'react-router-dom'
+import Player from './pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
 const App = () => {
+
   const navigate = useNavigate();
-  const location = useLocation();
-  const [authChecked, setAuthChecked] = useState(false);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setAuthChecked(true);
-
-      if (!user && location.pathname !== "/login") {
-        navigate("/login");
+    onAuthStateChanged(auth, async(user) => {
+      if(user) {
+        console.log("Logged In")
+        navigate('/')
+      } else {
+        console.log("Logged Out")
+        navigate('/login')
       }
-
-      if (user && location.pathname === "/login") {
-        navigate("/");
-      }
-    });
-
-    return () => unsubscribe();
-  }, []); 
-
-  if (!authChecked) return null;
+    })
+     return () => () => {}
+  }, [])
 
   return (
     <div>
-      <ToastContainer theme="dark" />
+      <ToastContainer theme='dark' />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/player/:id" element={<Player />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/player/:id' element={<Player/>} />
       </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App

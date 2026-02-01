5f94dc3e152583a0432148611132305c80927e23
Rosuelo996 <rosuelo996.sr@gmail.com>
2026-02-01 17:47:59 +0000

    fix navbar error

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,14 +12,23 @@ const Navbar = () => {
     const navRef = useRef();
 
     useEffect(() => {
-        window.addEventListener('scroll', () => {
-            if(window.scrollY >= 80) {
-                navRef.current.classList.add('nav-dark')
-            } else {
-                navRef.current.classList.remove('nav-dark')
-            }
-        })
-    }, [])
+  const handleScroll = () => {
+    if (!navRef.current) return;
+
+    if (window.scrollY >= 80) {
+      navRef.current.classList.add('nav-dark');
+    } else {
+      navRef.current.classList.remove('nav-dark');
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  return () => {
+    window.removeEventListener('scroll', handleScroll);
+  };
+}, []);
+
 
   return (
     <div ref={navRef} className='navbar'>
